82567a592e9e557a62b6598b0a07198a692a4ce0
Momin <global-apon.cse09@gmail.com>
2023-10-30 17:54:53 +0600

    Updated

--- redux-toolkit-app/index.js
+++ redux-toolkit-app/index.js
@@ -1,7 +1,51 @@
+const INCREMENT = "INCREMENT"
+const DECREMENT = "DECREMENT"
+const ADD_USER = "ADD_USER"
 // state
 const initialCounterState = {
     count : 0,
 }
 const initialUserState = {
-    users: [{name: "momin"}]
+    users: [{name: "Anisul Hoque"}]
 }
+
+//action object type payload
+const incrementCounter =() =>{
+    return {
+        type: INCREMENT,
+    }
+}
+const decrementCounter =() =>{
+    return {
+        type: DECREMENT,
+    }
+}
+const adduser =() =>{
+    return {
+        type: ADD_USER,
+        payload: {name:'momin'}
+    }
+}
+
+// create reducer for counter
+const counterReducer = (state=initialCounterState,action) =>
+{
+    switch(action.type){
+        case INCREMENT: 
+        return{
+            ...state,
+            count: state.count + 1
+        }
+       
+        case DECREMENT: return{
+            ...state,
+            count: state.count - 1
+        }
+        default: state;
+    }
+}
+
+// 1. state
+// 2. dispatch action
+// 3. reducer
+// 4. store update
